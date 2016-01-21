af224d04c870204738d86c944a99557e3280b33f
Marten Schilstra <mail@martenschilstra.nl>
2016-01-21 10:12:40 +0100

    Added display revisions

--- config/deploy.js
+++ config/deploy.js
@@ -12,7 +12,7 @@ module.exports = function(deployTarget) {
       activateOnDeploy: process.env['TRAVIS_BRANCH'] === 'master'
     },
 
-    s3: {
+    's3': {
       accessKeyId:     process.env['AWS_ACCESS_KEY_ID'],
       secretAccessKey: process.env['AWS_SECRET_ACCESS_KEY'],
       bucket:          process.env['AWS_BUCKET'],
